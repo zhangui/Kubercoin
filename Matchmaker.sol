de256e80c4d1c859e7e8a29b724fdd7a7df3234c
Twinov <hoeltgemarc@gmail.com>
2021-04-05 17:04:53 -0400

    contract closing compiling successfully

--- Matchmaker.sol
+++ Matchmaker.sol
@@ -1,30 +1,29 @@
 pragma solidity >=0.4.22 <0.6.0;
 
 contract Matchmaker {
-
     // matches potential machines/docker instances to clients
 
-    uint private verifierTimeInterval = 300;
-    uint private lastCheck;
+    uint256 private verifierTimeInterval = 300;
+    uint256 private lastCheck;
 
-    event MidContractTransfer(address payer, uint amount);
-    event ContractEnded(address payer, uint amount);
+    event MidContractTransfer(address payer, uint256 amount);
+    event ContractEnded(address payer, uint256 amount);
     event MinerListUpdate(address UpdatedMiner);
-    
-    constructor () {
-        lastCheck = now
+
+    constructor() public {
+        lastCheck = now;
     }
 
     struct ImageSet {
         ImageData[] images;
-        uint[] emptySlots;
+        uint256[] emptySlots;
     }
 
     struct ImageData {
         address owner;
-        uint costPerMinute;
-        uint maxTime;
-        uint startTime;
+        uint256 costPerMinute;
+        uint256 maxTime;
+        uint256 startTime;
         bool inUse;
         address currentClient;
     }
@@ -37,46 +36,45 @@ contract Matchmaker {
         // 1 true
         // 2 is undecided
         address[] addressList;
-        mapping(address => uint) verifications;
+        mapping(address => uint256) verifications;
     }
 
-    mapping(address => uint) minerRatings;
+    mapping(address => uint256) minerRatings;
     mapping(address => address[]) currentPings;
 
     mapping(address => string) publicKeys;
 
-
-
     mapping(address => Verifiers) pendingVerifies;
 
-    function checkVerifies() {
+    function checkVerifies() public {}
 
-    }
-
-    function checkForExpiriations() {
+    function checkForExpiriations() public {
         if (now - lastCheck >= verifierTimeInterval) {
-            checkVerifies()
+            checkVerifies();
         }
     }
 
-    function reportFailure(address minerAddress) {
+    function reportFailure(address minerAddress) public {}
 
-    }
-
-    function verify(address addressToBeVerified, bool active) {
-        address[] addressList = pendingVerifies[addressToBeVerified].addressList
-        for (uint i = 0; i < addressList.length; i++) {
+    function verify(address addressToBeVerified, bool active) public {
+        address[] storage addressList =
+            pendingVerifies[addressToBeVerified].addressList;
+        for (uint256 i = 0; i < addressList.length; i++) {
             if (msg.sender == addressList[i]) {
                 if (active) {
-                    pendingVerifies[addressToBeVerified].verifications[msg.sender] = 1;
+                    pendingVerifies[addressToBeVerified].verifications[
+                        msg.sender
+                    ] = 1;
                 } else {
-                    pendingVerifies[addressToBeVerified].verifications[msg.sender] = 0;
+                    pendingVerifies[addressToBeVerified].verifications[
+                        msg.sender
+                    ] = 0;
                 }
             }
         }
     }
 
-    function getPings() returns address[] {
+    function getPings() public {
         return currentPings[msg.sender];
     }
 
@@ -84,47 +82,61 @@ contract Matchmaker {
     //     clients.push(msg.sender);
     // }
 
-    function removeMiner() {
+    function removeMiner() public {}
 
-    }
-
-    function addMiner() {
+    function addMiner() public {
         if (imageStruct.emptySlots.length == 0) {
             imageStruct.images.push(msg.sender);
         } else {
             // check for race condition mutex lock maybe
             //make into queue
-            uint freeSlot = imageStruct.emptySlots[0]
-            imageStruct.images[]
+            uint256 freeSlot = imageStruct.emptySlots[0];
+            imageStruct.images[];
         }
     }
 
-
-
-    function closeConnection(uint position, address owner) {
-        ImageData data = imageList.images[position];
+    function closeConnection(uint256 position, address owner) public {
+        ImageData storage data = imageList.images[position];
         require(
             msg.sender == data.currentClient || msg.sender == data.owner,
             "You do not have access"
         );
-        if (data.currentClient == msg.sender) { //client is done with task
-            uint elapsedTime = now - data.startTime;
+        uint256 elapsedTime = now - data.startTime;
+        if (data.currentClient == msg.sender) {
+            //client is done with task
             data.inUse = false;
-            makeTransfer(owner, msg.sender, (elapsedTime / 60) * data.costPerMinute);
+            makeTransfer(
+                owner,
+                msg.sender,
+                (elapsedTime / 60) * data.costPerMinute
+            );
             emit ContractEnded(owner, msg.sender);
             imageList.emptySlots.push(position);
             emit MinerListUpdate(data.owner);
-        } else if (data.owner == msg.sender) { //miner decides to end contract
-            if (!data.inUse) { // task is complete
+        } else if (data.owner == msg.sender) {
+            //miner decides to end contract
+            if (!data.inUse) {
+                // task is complete
                 imageList.emptySlots.push(position);
                 emit MinerListUpdate(data.owner);
-                makeTransfer(owner, msg.sender, (elapsedTime / 60) * data.costPerMinute);
+                makeTransfer(
+                    owner,
+                    msg.sender,
+                    (elapsedTime / 60) * data.costPerMinute
+                );
                 emit ContractEnded(owner, msg.sender);
             } else {
                 //penalize miner and move funds for work done.
                 punishMiner(data.currentClient, data.owner);
-                emit MidContractTransfer(data.currentClient, (elapsedTime / 60) * data.costPerMinute);
-                makeTransfer(data.currentClient, data.owner, (elapsedTime / 60) * data.costPerMinute);
+                emit MidContractTransfer(
+                    data.currentClient,
+                    (elapsedTime / 60) * data.costPerMinute
+                );
+                makeTransfer(
+                    data.currentClient,
+                    data.owner,
+                    (elapsedTime / 60) * data.costPerMinute
+                );
 
                 //TODO: make sure removeImage and assignImage calls the MinterListUpdate event
                 removeImage(position);
@@ -133,16 +145,20 @@ contract Matchmaker {
         }
     }
 
-    function makeTransfer(address payer, address receiver, uint amount) {
-        payable(receiver).transfer(amount);
+    function makeTransfer(
+        address payer,
+        address receiver,
+        uint256 amount
+    ) public {
+        receiver.transfer(amount);
     }
 
     //can probably remove the below 2 functions in the future, but might add additional functionality
-    function punishMiner(address miner) {
-      reduceRating(miner);
+    function punishMiner(address miner) public {
+        reduceRating(miner);
     }
 
-    function punishPing(address pinger) {
+    function punishPing(address pinger) public {
         reduceRating(pinger);
     }
 }
